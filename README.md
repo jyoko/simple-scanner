4f3568bfb7e0f77fcb66f9fadc913c0bf8e80560
jyoko <jyoko@users.noreply.github.com>
2016-06-30 18:14:45 -0400

    UDP scan (experimental)
    * Some other cleanup/refactors as well
    * 'maybe' addition to wordpress scan
    * Found a raw-socket library, UDP scan to test it out!

--- README.md
+++ README.md
@@ -1,49 +1,61 @@
-# Simple Scanner (revised)
+# Simple Scanner
 
-Naively identify core server utilities and scan for Wordpress with no dependencies in Node!  Warning: This is a **NOISY** scanner and you can easily cause network issues or get yourself blocked.
+Not as simple anymore -- slowly building a fully-featured port scanner with various enumeration features in Node!  Warning: This can easily be a **NOISY** scan and cause network issues or upset a host (and the usual scanner/open source caveats).
 
 ## Usage
 
-Requires modern Node (4+?), uses template strings, arrow functions, `class`, and default params (possibly others, but modern v8 compatible).
+Requires modern Node (4+?), uses template strings, arrow functions, `class`, and default params (possibly others, but modern v8 compatible). Note that if you want to use the UDP scan and other experimental scanning techniques that require raw sockets, you may need to have elevated privileges. If you're using nvm/n your system version of Node (used by sudo) may not support these features, type `which node` and use that one directly.
 
 ```sh
 $ npm install
 ```
 
-For [request](https://github.com/request/request) used in WPScanner and [commander](https://github.com/tj/commander.js) used in example-scan.
+For [request](https://github.com/request/request) used in WPScanner, [commander](https://github.com/tj/commander.js) used in the CLI (simple-scanner.js), and [raw-socket](https://github.com/stephenwvickers/node-raw-socket) for the UDP scan and other incoming types.
 
 ```sh
-$ ./example-scanner.js -h scanthissite.com
+$ ./simple-scanner.js scanthissite.com
 ```
 
 Full help text:
 
 ```sh
-  Usage: example-scan [options]
+  Usage: simple-scanner <host> [options]
 
   Options:
 
     -h, --help               output usage information
     -V, --version            output the version number
-    -h, --host <host>        The host to scan
     -p, --ports [22,80,443]  Enter a comma-delimited list of port numbers (will override range)
     -r, --range [1-1000]     Enter a range of ports (default: 1-1000)
     -i, --interval [1]       For use with --range, enter an interval (default: 1)
     -m, --maxParallel [12]   Max parallel connections
+    -w, --wordpress          Do only wordpress probe on indicated ports
+    -b, --baseuri [wp]       URI to use as base for wordpress probe
+    -U, --udp                Do only UDP scan - WARNING: experimental, slow, likely requires root
+    -C, --connect            Do only connect scan
     -v, --verbose            More status updates
+
+  Default scan is a TCP Connect on ports 1-1000 using Node's Net library,
+  any open ports that do not respond with a banner will be probed for an HTTP
+  server and Wordpress instances.
+
+  Other scan types are experimental and may not work as-intended on all
+  operating systems.
 ```
 
 ## Scanning Methods
 
-The example script first runs a connect scan (using Node's built-in net) over the entire range of ports and saves any banners sent from the server during this probe. From the initial scan, any open ports that _did not_ send a banner will be probed via HTTP(S) for Wordpress markers.
+The default scan first runs a connect scan (using Node's built-in net) over the entire range of ports and saves any banners sent from the server during this probe. From the initial scan, any open ports that _did not_ send a banner will be probed via HTTP(S) for Wordpress markers.
 
-## Fingerprinting & Detection 
+## Connect -C
 
-The methods used are extremely basic, but mostly effective.
+Uses Node's Net library to attempt to connect to specified ports on the host. Reports any open ports and banners sent.
 
-Any self-identifying server banner will be reported and the initial Server HTTP header response is saved to avoid false information from later probes. Additionally, the WPScanner uses an iPhone user agent to avoid simple bot detection.
+Code in `lib/PortScanner.js`
 
-WPScanner attempts to confirm a Wordpress server via:
+## Wordpress -w
+
+Uses request to probe for HTTP(S) servers and attempts to enumerate a Wordpress instance via:
 
 * Meta generator tag
 * "Powered By Wordpress"
@@ -52,10 +64,32 @@ WPScanner attempts to confirm a Wordpress server via:
 * the existence of /wp-admin or /wp-login.php
 * a license.txt or readme.html containing the string "wordpress"
 
-In the next update there will be more detection methods and the WPScanner will be cleaned up, the original didn't transfer well.
+Code in `lib/WPProbe.js`
+
+## UDP -U
+
+**Requires privileged user**
+
+Uses Node's built-in dgram to send UDP datagrams and listen for responses and raw-socket to listen for ICMP response codes. This is ripped directly from [nmap's -sU scan description](https://nmap.org/book/man-port-scanning-techniques.html).
+
+`lib/ICMPWatcher` contains the code that looks for relevant ICMP replies, `lib/UDPScanner` sets up the dgram listener and sends the packets.
+
+## Fingerprinting & Detection 
+
+The methods used are extremely basic but mostly effective.
+
+Any self-identifying server banner will be reported and any Server HTTP header response is saved. Additionally, the WPScanner uses an iPhone user agent to avoid simple bot detection.
+
+More, better detection will be incoming along with the scanning methods we've all come to know and love.
 
 ## Known Issues
 
 * No support for scanning multiple hosts
 * No backoff and retries, sometimes leading to bad results (imagine throwing rocks to see if someone left any windows open)
+* Unknown cross-platform support for raw-socket
 
+## Contributing
+
+Hopefully the code itself is readable and commented, the main architectural thing to be aware of is all scan types should inherit from the base class in `lib/Scanner`. This lets them be EventEmitters and lets you focus on writing a scan from the perspective of a single check (as well as making the interface to the UI consistent). 
+
+If the method itself is complicated (or has involved parts) by all means move that to a standalone section and write an XXScaner to mediate.
---
+++ simple-scanner.js
@@ -0,0 +1,271 @@
+#!/usr/bin/env node
+
+/*
+ * simple-scanner CLI
+ *
+ * Uses commander for easy access to basic scanning usages.
+ *
+ * Will likely be expanded to mimic more familiar scanners (ie: nmap)
+ *
+ * TODO: Better support for verbose logging, give full scan result (closed&filtered)
+ */
+
+var program = require('commander');
+var PortScanner = require('./lib/PortScanner');
+var WPScanner = require('./lib/WPScanner');
+var UDPScanner = require('./lib/UDPScanner');
+var utilities = require('./lib/utilities');
+var defaultScan = true;
+
+program
+  .version('0.4.0')
+  .usage('<host> [options]')
+  .option('-p, --ports [22,80,443]', 'Enter a comma-delimited list of port numbers (will override range)', portsFromList)
+  .option('-r, --range [1-1000]', 'Enter a range of ports (default: 1-1000)', portsFromRange,{start:1,end:1000})
+  .option('-i, --interval [1]', 'For use with --range, enter an interval (default: 1)', validPositiveNumber)
+  .option('-m, --maxParallel [12]', 'Max parallel connections', validPositiveNumber)
+  .option('-w, --wordpress', 'Do only wordpress probe on indicated ports', false)
+  .option('-b, --baseuri [wp]', 'URI to use as base for wordpress probe', '')
+  .option('-U, --udp', 'Do only UDP scan - WARNING: experimental, slow, likely requires root', false)
+  .option('-C, --connect', 'Do only connect scan', false)
+// TODO .option('--randomize', 'Randomize port order (default is sequential)', false);
+  .option('-v, --verbose', 'More status updates', false);
+
+program.on('--help', function() {
+  console.log(
+`  Default scan is a TCP Connect on ports 1-1000 using Node's Net library,
+  any open ports that do not respond with a banner will be probed for an HTTP
+  server and Wordpress instances.
+
+  Other scan types are experimental and may not work as-intended on all
+  operating systems.
+  `);
+});
+
+program.parse(process.argv);
+
+program.host = program.args[0];
+
+if (!program.host) {
+  program.help();
+}
+
+/*
+ * Utility functions
+ *
+ */
+
+function portsFromList(str) {
+  return utilities.filterValidPorts(str.split(','));
+}
+
+function portsFromRange(str) {
+  var [start,end] = str.split('-');
+  return {start:parseInt(start)||1,end:parseInt(end)||1000};
+}
+
+function validPositiveNumber(n) {
+  n = parseInt(n);
+  return (isNaN(n) || n<1) ? 1 : n;
+}
+
+// for timing info
+var startTime;
+function secondsFromNow(time) {
+  return Math.round((Date.now()-time)/1000);
+}
+
+// for rearranging scan results
+function portSort(a,b) {
+  return a.port-b.port;
+}
+
+function onError(e) {
+  console.log('ERROR: ');
+  console.log(e);
+}
+
+// not-particularly-sophisticated
+function statusUpdate(scanHeader, length, alertOnOpen) {
+  var count = 0;
+  var percent = 0;
+  var previous = 0;
+  var jumpToDisplay = program.verbose? 4 : 19;
+  return function(result) {
+    count++;
+    percent = Math.floor(count/length*100);
+    if (percent-previous > jumpToDisplay) {
+      console.log(`${scanHeader}: ${percent}% complete`);
+      previous = percent;
+    }
+    if (alertOnOpen && result.data.status === 'open') {
+      console.log(`OPEN: port ${result.port}`);
+    }
+  };
+};
+
+function prettyPrint(o) {
+  // o = {port: num, data: {wordpress: bool, server: str}}
+
+  var pad = (n,s)=>s.length<n?(s+Array(n+1).join(' ')).substr(0,n):s;
+  var p10 = pad.bind(0,10);
+
+  var output = p10(o.port.toString())+' : ';
+  for (var k in o.data) {
+    // in case of multiple servers reported by wpScan
+    if (Array.isArray(o.data[k])) {
+      keyValue = o.data[k].join(' & ');
+    } else {
+      keyValue = o.data[k];
+    }
+
+    output += `${pad(12,k)} : ${keyValue}\n`;
+    output += pad(13,'');
+  }
+  console.log(output);
+}
+
+/*
+ * The actual scanning
+ *
+ */
+
+var config = {
+  host: program.host,
+  ports: program.ports,
+  start: program.range.start,
+  end: program.range.end,
+  interval: program.interval,
+  maxParallel: program.maxParallel,
+};
+
+
+if (program.wordpress) {
+  defaultScan = false;
+  doWpScan();
+} 
+
+if (program.udp) {
+  defaultScan = false;
+  doUdpScan();
+}
+
+if (program.connect) {
+  defaultScan = false;
+  doConnectScan();
+}
+
+if (defaultScan) {
+  doConnectScan();
+}
+
+function doConnectScan() {
+
+  var portScan = new PortScanner(config);
+
+  portScan.on('error', onError);
+
+  portScan.on('ready', function() {
+    console.log('Starting connect scan');
+    startTime = Date.now();
+    portScan.scan();
+  });
+
+  portScan.on('portFinished', statusUpdate('CONNECT SCAN', portScan.portList.length, true));
+      
+  portScan.on('complete', function(results) {
+
+    console.log(`Connect scan completed in ${secondsFromNow(startTime)} seconds`);
+
+    var openPorts = results.filter(_=>_.data.status==='open').sort(portSort);
+
+    if (!openPorts.length) {
+      console.log('No open ports found');
+      return;
+    }
+
+    if (defaultScan) {
+      var sorted = openPorts.reduce(function(obj, result) {
+        if (result.data.response) {
+          obj.noScan.push({port:result.port,data:{
+            wordpress:false,server:result.data.response.trim()
+          }});
+        } else {
+          obj.toScan.push(result.port);
+        }
+        return obj;
+      }, {toScan:[],noScan:[]});
+      doWpScan(sorted);
+    } else {
+      openPorts.forEach(prettyPrint);
+    }
+  });
+}
+
+function doWpScan(previousResults) {
+
+  if (previousResults) {
+    if (!previousResults.toScan.length) {
+      console.log('No HTTP servers to probe');
+      console.log('Banner results:');
+      previousResults.noScan.forEach(prettyPrint);
+      return;
+    }
+
+    config.ports = previousResults.toScan;
+  }
+
+  if (program.baseuri) {
+    config.baseUri = program.baseuri;
+  }
+
+  var wpScan = new WPScanner(config);
+
+  wpScan.on('error', onError);
+
+  wpScan.on('ready', function() {
+    startTime = Date.now();
+    console.log('Starting WP probe');
+    wpScan.scan();
+  });
+
+  wpScan.on('portFinished', statusUpdate('WP SCAN', wpScan.portList.length));
+
+  wpScan.on('complete', function(results) {
+    console.log(`WP scan completed in ${secondsFromNow(startTime)} seconds`);
+    console.log(`Results for ${program.host}:`);
+    var finalResults;
+    if (previousResults) {
+      finalResults = results.concat(previousResults.noScan).sort(portSort);
+    } else {
+      finalResults = results.filter(o=>o.data.wordpress).sort(portSort);
+      if (!finalResults.length) {
+        console.log('No wordpress instances found');
+      }
+    }
+    finalResults.forEach(prettyPrint);
+  });
+}
+
+// experimental, bear with me
+function doUdpScan() {
+
+  var udpScan = new UDPScanner(config);
+  udpScan.on('error', function(e) {
+    if (e.message === 'Operation not permitted') {
+      console.log('\nUDP scan requires elevated privileges, try sudo\n');
+      process.exit();
+    }
+    console.log('ERROR: ');
+    console.log(e);
+  });
+  udpScan.on('icmpready', function() {
+    udpScan.scan();
+  });
+  udpScan.on('portFinished', statusUpdate('UDP SCAN', udpScan.portList.length));
+  udpScan.on('complete', function(results) {
+    results.forEach(prettyPrint);
+  });
+
+}
+
