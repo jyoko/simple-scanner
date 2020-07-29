7ae63cc7553c93a1c9dcc2eb7781f24ca648fa51
jyoko <jyoko@users.noreply.github.com>
2020-07-29 14:56:13 -0400

    Refactor entry point
    * Less copy/paste, simpler control flow
    * Some syntax updates for newer Node LTS

--- simple-scanner.js
+++ simple-scanner.js
@@ -10,45 +10,57 @@
  * TODO: Better support for verbose logging, give full scan result (closed&filtered)
  */
 
-var program = require('commander');
-var PortScanner = require('./lib/PortScanner');
-var WPScanner = require('./lib/WPScanner');
-var UDPScanner = require('./lib/UDPScanner');
-var SYNScanner = require('./lib/SYNScanner');
-var NULLScanner = require('./lib/NULLScanner');
-var FINScanner = require('./lib/FINScanner');
-var XmasScanner = require('./lib/XmasScanner');
-var ACKScanner = require('./lib/ACKScanner');
-var utilities = require('./lib/utilities');
-var defaultScan = true;
+const program         = require('commander');
+const ConnectScanner  = require('./lib/ConnectScanner');
+const WPScanner       = require('./lib/WPScanner');
+const UDPScanner      = require('./lib/UDPScanner');
+const SYNScanner      = require('./lib/SYNScanner');
+const NULLScanner     = require('./lib/NULLScanner');
+const FINScanner      = require('./lib/FINScanner');
+const XmasScanner     = require('./lib/XmasScanner');
+const ACKScanner      = require('./lib/ACKScanner');
+const utilities       = require('./lib/utilities');
+const log             = require('./lib/log');
+
+const scanners = {
+  connect: ConnectScanner,
+  wordpress: WPScanner,
+  udp: UDPScanner,
+  syn: SYNScanner,
+  null: NULLScanner,
+  fin: FINScanner,
+  xmas: XmasScanner,
+  ack: ACKScanner,
+};
+
+let defaultScan = true;
 
 program
-  .version('0.6.0')
+  .version('0.7.0')
   .usage('<host> [options]')
   .option('-p, --ports [22,80,443]', 'Enter a comma-delimited list of port numbers (will override range)', portsFromList)
   .option('-r, --range [1-1000]', 'Enter a range of ports (default: 1-1000)', portsFromRange,{start:1,end:1000})
   .option('-i, --interval [1]', 'For use with --range, enter an interval (default: 1)', validPositiveNumber)
   .option('-m, --maxParallel [12]', 'Max parallel connections', validPositiveNumber)
-  .option('-w, --wordpress', 'Do only wordpress probe on indicated ports', false)
+  .option('-t, --timeout [2000]', 'Time to wait for response from target', validPositiveNumber)
+  //.option('-w, --wordpress', 'Do only wordpress probe on indicated ports', false)
   .option('-b, --baseuri [wp]', 'URI to use as base for wordpress probe', '')
   .option('-C, --connect', 'Do only connect scan', false)
-  .option('-U, --udp', 'Do only UDP scan - WARNING: experimental, slow, likely requires root', false)
-  .option('-S, --syn', 'Do only SYN (half-open) scan - WARNING: experimental, in-progress', false)
-  .option('-N, --null', 'Do only NULL scan - WARNING: experimental', false)
-  .option('-F, --fin', 'Do only FIN scan - WARNING: experimental', false)
-  .option('-X, --xmas', 'Do only Xmas scan - WARNING: experimental', false)
-  .option('-A, --ack', 'Do only ACK scan - WARNING: experimental', false)
+  .option('-U, --udp', 'Do only UDP scan - requires raw-socket', false)
+  .option('-S, --syn', 'Do only SYN (half-open) scan - requires raw-socket', false)
+  .option('-N, --null', 'Do only NULL scan - requires raw-socket', false)
+  .option('-F, --fin', 'Do only FIN scan - requires raw-socket', false)
+  .option('-X, --xmas', 'Do only Xmas scan - requires raw-socket', false)
+  .option('-A, --ack', 'Do only ACK scan - requires raw-socket', false)
 // TODO .option('--randomize', 'Randomize port order (default is sequential)', false);
+  .option('-6, --ipv6', 'Use IPv6', false)
+  .option('-4, --ipv4', 'Use IPv4', false)
   .option('-v, --verbose', 'More status updates', false);
 
 program.on('--help', function() {
-  console.log(
-`  Default scan is a TCP Connect on ports 1-1000 using Node's Net library,
-  any open ports that do not respond with a banner will be probed for an HTTP
-  server and Wordpress instances.
-
-  Other scan types are experimental and may not work as-intended on all
-  operating systems.
+  console.log(`
+  Default scan is a TCP Connect on ports 1-1000 using Node's Net library.
+  HTTP probes are currently disabled.
   `);
 });
 
@@ -58,6 +70,17 @@ program.host = program.args[0];
 
 if (!program.host) {
   program.help();
+}
+
+if (program.host === 'localhost' || program.host === '127.0.0.1' || program.host === '::1') {
+  // concatenation because a single backtick string causes odd behavior in editor
+  console.log(
+    '\n'+
+    'Scanning yourself via loopback (localhost/127.0.0.1/::1) may not work,\n'+
+    'try your network addresses:\n'+
+    `  IPv4: ${utilities.getLocalIP(4)}`+
+    `  IPv6: ${utilities.getLocalIP(6)}\n`
+  );
 }
 
 /*
@@ -70,7 +93,7 @@ function portsFromList(str) {
 }
 
 function portsFromRange(str) {
-  var [start,end] = str.split('-');
+  const [start,end] = str.split('-');
   return {start:parseInt(start)||1,end:parseInt(end)||1000};
 }
 
@@ -80,7 +103,6 @@ function validPositiveNumber(n) {
 }
 
 // for timing info
-var startTime;
 function secondsFromNow(time) {
   return Math.round((Date.now()-time)/1000);
 }
@@ -91,28 +113,27 @@ function portSort(a,b) {
 }
 
 function onError(e) {
-  console.log('ERROR: ');
-  console.log(e);
+  log.error(e);
 }
 
 // not-particularly-sophisticated
 function statusUpdate(scanHeader, length, alertOnOpen) {
-  var count = 0;
-  var percent = 0;
-  var previous = 0;
-  var jumpToDisplay = program.verbose? 4 : 19;
+  let count = 0;
+  let percent = 0;
+  let previous = 0;
+  const jumpToDisplay = program.verbose? 4 : 19;
   return function(result) {
     count++;
     percent = Math.floor(count/length*100);
     if (percent-previous > jumpToDisplay) {
-      console.log(`${scanHeader}: ${percent}% complete`);
+      log.info(`${scanHeader}: ${percent}% complete`);
       previous = percent;
     }
     if (alertOnOpen && result.data.status === 'open') {
-      console.log(`OPEN: port ${result.port}`);
+      log.info(`OPEN: port ${result.port}`);
     }
   };
-};
+}
 
 function prettyPrint(o) {
   // o = {port: num, data: {wordpress: bool, server: str}}
@@ -140,69 +161,54 @@ function prettyPrint(o) {
  *
  */
 
-var config = {
+const config = {
   host: program.host,
   ports: program.ports,
   start: program.range.start,
   end: program.range.end,
   interval: program.interval,
   maxParallel: program.maxParallel,
+  timeout: program.timeout,
+  ipFamily: program.ipv6 ? 6 : (program.ipv4 ? 4 : 0),
 };
 
+const scan = Object.keys(scanners).find(name=>{
+  if (program[name]) doScan(scanners[name]);
+});
 
-// TODO: this flow control
-if (program.wordpress) {
-  defaultScan = false;
-  doWpScan();
-} 
+if (!scan) doScan(ConnectScanner);
 
-if (program.udp) {
-  defaultScan = false;
-  doUdpScan();
-}
-
-if (program.connect) {
-  defaultScan = false;
-  doConnectScan();
-}
-
-if (program.syn) {
-  defaultScan = false;
-  doSynScan();
-}
-
-if (program.null) {
-  defaultScan = false;
-  doNullScan();
-}
-
-if (program.fin) {
-  defaultScan = false;
-  doFinScan();
-}
-
-if (program.xmas) {
-  defaultScan = false;
-  doXmasScan();
-}
-
-if (program.ack) {
-  defaultScan = false;
-  doAckScan();
-}
-
-if (defaultScan) {
-  doConnectScan();
+function doScan(Scanner) {
+  const scanner = new Scanner(config);
+  let startTime;
+  scanner.on('error', function(e) {
+    if (e.message === 'Operation not permitted') {
+      log.warn('Scan requires elevated privileges, try it with sudo');
+      process.exit();
+    } else {
+      log.error(e);
+    }
+  });
+  scanner.on('ready', function() {
+    startTime = Date.now();
+    scanner.scan();
+  });
+  scanner.on('portFinished', statusUpdate('SCAN', scanner.portList.length));
+  scanner.on('complete', function(results) {
+    log.info(`Scan completed in ${secondsFromNow(startTime)} seconds`);
+    results.forEach(prettyPrint);
+  });
 }
 
 function doConnectScan() {
 
-  var portScan = new PortScanner(config);
+  let portScan = new PortScanner(config);
+  let startTime;
 
   portScan.on('error', onError);
 
   portScan.on('ready', function() {
-    console.log('Starting connect scan');
+    log.info('Starting connect scan');
     startTime = Date.now();
     portScan.scan();
   });
@@ -211,12 +217,12 @@ function doConnectScan() {
       
   portScan.on('complete', function(results) {
 
-    console.log(`Connect scan completed in ${secondsFromNow(startTime)} seconds`);
+    log.info(`Connect scan completed in ${secondsFromNow(startTime)} seconds`);
 
     var openPorts = results.filter(_=>_.data.status==='open').sort(portSort);
 
     if (!openPorts.length) {
-      console.log('No open ports found');
+      log.info('No open ports found');
       return;
     }
 
@@ -242,14 +248,16 @@ function doWpScan(previousResults) {
 
   if (previousResults) {
     if (!previousResults.toScan.length) {
-      console.log('No HTTP servers to probe');
-      console.log('Banner results:');
+      log.info('No HTTP servers to probe');
+      log.info('Banner results:');
       previousResults.noScan.forEach(prettyPrint);
       return;
     }
 
     config.ports = previousResults.toScan;
   }
+
+  let startTime;
 
   if (program.baseuri) {
     config.baseUri = program.baseuri;
@@ -261,235 +269,25 @@ function doWpScan(previousResults) {
 
   wpScan.on('ready', function() {
     startTime = Date.now();
-    console.log('Starting WP probe');
+    log.info('Starting WP probe');
     wpScan.scan();
   });
 
   wpScan.on('portFinished', statusUpdate('WP SCAN', wpScan.portList.length));
 
   wpScan.on('complete', function(results) {
-    console.log(`WP scan completed in ${secondsFromNow(startTime)} seconds`);
-    console.log(`Results for ${program.host}:`);
+    log.info(`WP scan completed in ${secondsFromNow(startTime)} seconds`);
+    log.info(`Results for ${program.host}:`);
     var finalResults;
     if (previousResults) {
       finalResults = results.concat(previousResults.noScan).sort(portSort);
     } else {
       finalResults = results.filter(o=>o.data.wordpress).sort(portSort);
       if (!finalResults.length) {
-        console.log('No wordpress instances found');
+        log.info('No wordpress instances found');
       }
     }
     finalResults.forEach(prettyPrint);
   });
 }
 
-// experimental, bear with me
-function doUdpScan() {
-
-  var udpScan = new UDPScanner(config);
-  udpScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nUDP scan requires elevated privileges, try sudo\n');
-      process.exit();
-    }
-    console.log('ERROR: ');
-    console.log(e);
-  });
-  udpScan.on('icmpready', function() {
-    udpScan.scan();
-  });
-  udpScan.on('portFinished', statusUpdate('UDP SCAN', udpScan.portList.length));
-  udpScan.on('complete', function(results) {
-    results.forEach(prettyPrint);
-  });
-
-}
-
-// see above :-D
-function doSynScan() {
-  console.log(`
-Scanning yourself via loopback (localhost/127.0.0.1) may not work,
-try your network address - ${utilities.getLocalIP()}
-`);
-  var synScan = new SYNScanner(config);
-  synScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nSYN scan requires elevated privileges, try sudo\n');
-      process.exit();
-    } else {
-      console.log('ERROR: ');
-      console.log(e);
-    }
-  });
-  synScan.on('icmpready', function() {
-    synScan.scan();
-  });
-  synScan.on('portFinished', statusUpdate('SYN SCAN', synScan.portList.length));
-  synScan.on('complete', function(results) {
-    // holy ugly results TODO
-    var filteredResults = results.reduce((o,result)=>{
-      if (result.data.status === 'closed') {
-        o[0].data.closed++;
-      }
-      if (result.data.status === 'filtered') {
-        o[1].data.filtered++;
-      }
-      if (result.data.status === 'open') {
-        o.push(result);
-      }
-      return o;
-    }, [{port:'combined',data:{closed:0}},{port:'combined',data:{filtered:0}}]);
-
-    filteredResults.forEach(prettyPrint);
-  });
-}
-
-function doNullScan() {
-  console.log(`
-Scanning yourself via loopback (localhost/127.0.0.1) may not work,
-try your network address - ${utilities.getLocalIP()}
-`);
-  var nullScan = new NULLScanner(config);
-  nullScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nNULL scan requires elevated privileges, try sudo\n');
-      process.exit();
-    } else {
-      console.log('ERROR: ');
-      console.log(e);
-    }
-  });
-  nullScan.on('icmpready', function() {
-    nullScan.scan();
-  });
-  nullScan.on('portFinished', statusUpdate('NULL SCAN', nullScan.portList.length));
-  nullScan.on('complete', function(results) {
-    // holy ugly results TODO
-    var filteredResults = results.reduce((o,result)=>{
-      if (result.data.status === 'closed') {
-        o[0].data.closed++;
-      }
-      if (result.data.status === 'filtered') {
-        o[1].data.filtered++;
-      }
-      if (result.data.status === 'open|filtered') {
-        o.push(result);
-      }
-      return o;
-    }, [{port:'combined',data:{closed:0}},{port:'combined',data:{filtered:0}}]);
-
-    filteredResults.forEach(prettyPrint);
-  });
-}
-
-function doFinScan() {
-  console.log(`
-Scanning yourself via loopback (localhost/127.0.0.1) may not work,
-try your network address - ${utilities.getLocalIP()}
-`);
-  var finScan = new FINScanner(config);
-  finScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nNULL scan requires elevated privileges, try sudo\n');
-      process.exit();
-    } else {
-      console.log('ERROR: ');
-      console.log(e);
-    }
-  });
-  finScan.on('icmpready', function() {
-    finScan.scan();
-  });
-  finScan.on('portFinished', statusUpdate('NULL SCAN', finScan.portList.length));
-  finScan.on('complete', function(results) {
-    // holy ugly results TODO
-    var filteredResults = results.reduce((o,result)=>{
-      if (result.data.status === 'closed') {
-        o[0].data.closed++;
-      }
-      if (result.data.status === 'filtered') {
-        o[1].data.filtered++;
-      }
-      if (result.data.status === 'open|filtered') {
-        o.push(result);
-      }
-      return o;
-    }, [{port:'combined',data:{closed:0}},{port:'combined',data:{filtered:0}}]);
-
-    filteredResults.forEach(prettyPrint);
-  });
-}
-
-
-function doXmasScan() {
-  console.log(`
-Scanning yourself via loopback (localhost/127.0.0.1) may not work,
-try your network address - ${utilities.getLocalIP()}
-`);
-  var xmasScan = new XmasScanner(config);
-  xmasScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nNULL scan requires elevated privileges, try sudo\n');
-      process.exit();
-    } else {
-      console.log('ERROR: ');
-      console.log(e);
-    }
-  });
-  xmasScan.on('icmpready', function() {
-    xmasScan.scan();
-  });
-  xmasScan.on('portFinished', statusUpdate('NULL SCAN', xmasScan.portList.length));
-  xmasScan.on('complete', function(results) {
-    // holy ugly results TODO
-    var filteredResults = results.reduce((o,result)=>{
-      if (result.data.status === 'closed') {
-        o[0].data.closed++;
-      }
-      if (result.data.status === 'filtered') {
-        o[1].data.filtered++;
-      }
-      if (result.data.status === 'open|filtered') {
-        o.push(result);
-      }
-      return o;
-    }, [{port:'combined',data:{closed:0}},{port:'combined',data:{filtered:0}}]);
-
-    filteredResults.forEach(prettyPrint);
-  });
-}
-
-function doAckScan() {
-  console.log(`
-Scanning yourself via loopback (localhost/127.0.0.1) may not work,
-try your network address - ${utilities.getLocalIP()}
-`);
-  var ackScan = new ACKScanner(config);
-  ackScan.on('error', function(e) {
-    if (e.message === 'Operation not permitted') {
-      console.log('\nNULL scan requires elevated privileges, try sudo\n');
-      process.exit();
-    } else {
-      console.log('ERROR: ');
-      console.log(e);
-    }
-  });
-  ackScan.on('icmpready', function() {
-    ackScan.scan();
-  });
-  ackScan.on('portFinished', statusUpdate('NULL SCAN', ackScan.portList.length));
-  ackScan.on('complete', function(results) {
-    // holy ugly results TODO
-    var filteredResults = results.reduce((o,result)=>{
-      if (result.data.status === 'filtered') {
-        o[0].data.filtered++;
-      }
-      if (result.data.status === 'unfiltered') {
-        o.push(result);
-      }
-      return o;
-    }, [{port:'combined',data:{filtered:0}}]);
-
-    filteredResults.forEach(prettyPrint);
-  });
-}
